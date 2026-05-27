f77ba9948aa8d9100a267130e4a75f9b0a8f154c
bogofiat <dmaxgw2548@gmail.com>
2026-05-28 00:48:06 +0700

    Remove unnesscery files

--- model/CNN_model.py
+++
@@ -1,7 +0,0 @@
-import mediapipe as mp
-import numpy as np
-import tensorflow as tf
-import os 
-from sklearn.model_selection import train_test_split    
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
